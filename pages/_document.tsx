2ec7b6c0d8b29abdc86cb73415c94c2ef141bf5e
Ian Davis <jungziege@gmail.com>
2022-12-27 11:28:01 -0800

    Remove unused stylesheet

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link rel="stylesheet" href="https://use.typekit.net/kaw3tvf.css" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
