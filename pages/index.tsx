03684027b6620d6306ecdd2612fd27d977f62f58
Ian Davis <jungziege@gmail.com>
2022-12-26 17:06:23 -0800

    Add basic prose page structure
    Copied from github.com/mysilio-co/exquisite-garden

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,43 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Link from "next/link";
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+export default function DecentSocialLandingPage() {
   return (
-    <>
-      <Head>
-        <title>DecentSocial</title>
-        <meta name="description" content="a social about social" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            DecentSocial is a gathering to discuss the social web, and you're invited.
+    <main className="min-h-screen bg-gradient-to-r from-my-green via-ocean to-my-purple">
+      <section className="min-h-screen relative mx-auto w-4/5 bg-white/90 shadow-2xl py-20">
+        <div className="text-lg max-w-prose mx-auto">
+          <h1>
+            <span className="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+              DecentSocial 2023
+            </span>
+          </h1>
+          <p className="mt-8 text-xl text-gray-500 leading-8">
+            DecentSocial is a digital unconference for the builders of the decentralized social web.
           </p>
         </div>
-      </main>
-    </>
-  )
+        <div className="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
+          <div className="text-center">
+              <h2>
+                <a href="https://opencollective.com/decentsocial/events/decentsocial-conference-8c7e0a05/contribute/decentsocial-2023-49790">Regsiter for DecentSocial 2023</a>
+              </h2>
+          </div>
+          <h2>Who is DecentSocial for?</h2>
+          <p>
+            TODO
+          </p>
+          <h2>How do I attend?</h2>
+          <p>
+            TODO
+          </p>
+          <h2>How much does DecentSocial cost?</h2>
+          <p>
+            TODO
+          </p>
+          <h2>How is DecentSocial funded?</h2>
+          <p>
+            TODO
+          </p>
+        </div>
+      </section>
+    </main>
+  );
 }
