b574d1b2cf513f49ff669d45f0d0931aa07222d0
Ian Davis <jungziege@gmail.com>
2022-12-26 16:00:58 -0800

    Try out new deployment process

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>decent social</title>
+        <title>DecentSocial</title>
         <meta name="description" content="a social about social" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
