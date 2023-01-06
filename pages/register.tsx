39cb57a11b2e12ae25be7d5a4cd287995a9496d8
Ian Davis <jungziege@gmail.com>
2023-01-06 11:16:07 +0000

    NextJS redirects don't work with Coderberg pages
    Our hacky codeberg deployment flow means setting up redirects in the
    next config doesn't actually work, so just redirect manually via js
    on the register page.

---
+++ pages/register.tsx
@@ -0,0 +1,11 @@
+import {useEffect} from 'react'
+
+export default function redirect() {
+    useEffect(() => {
+        window.location.assign('https://opencollective.com/decentsocial/events/decentsocial-conference-8c7e0a05/contribute/decentsocial-2023-49790')
+    })
+    return(
+        <>
+        </>
+    )
+}
