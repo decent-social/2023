dc3341f2b755b53a0142a83e6dd717fa93a26d72
Ian Davis <jungziege@gmail.com>
2023-01-09 19:00:46 +0000

    Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,15 +8,11 @@ if ! [ -z "$(git status --porcelain)" ]; then
 fi
 
 npm run build
-cp -r out/* decentsocial.codeberg.page/
 GIT_SHA=$(git rev-parse --short HEAD)
 pushd decentsocial.codeberg.page
 
-SUBPROJECT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-if [[ "$SUBPROJECT_BRANCH" != "main" ]]; then
-  echo 'Aborting deployment. decentsocial.codeberg.page subproject is not on main branch.';
-  exit 1;
-fi
+git checkout main
+cp -r ../out/* .
 git add -A .
 git status
 
@@ -34,3 +30,5 @@ case "$response" in
 esac
 
 popd
+
+git commit -m "Deployed $GIT_SHA to DecentSocial/pages"
