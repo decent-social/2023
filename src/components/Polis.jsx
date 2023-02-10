7737368650027fbee338df27bf0760f0d3ab1b37
Ian Davis <jungziege@gmail.com>
2023-02-10 16:12:00 +0000

    Update polis url to point to our poll

--- src/components/Polis.jsx
+++ src/components/Polis.jsx
@@ -11,7 +11,7 @@ export function Polis() {
         <div className="mx-auto max-w-2xl lg:max-w-4xl lg:px-12">
           <h3 className="font-display text-3xl font-bold tracking-tighter text-witch-purple">
             You can also participate asyncronously using{' '}
-            <a href="pol.is">Polis</a>
+            <a href="https://pol.is/5iw6kcubty">Polis</a>
           </h3>
           <div className="polis pt-12" data-conversation_id="5iw6kcubty"></div>
         </div>
