f26587915b32dbc2439d5212dfd479a0dc2efd82
Ian Davis <jungziege@gmail.com>
2023-02-10 15:04:44 +0000

    Move Images to src; create Room components

---
+++ src/components/Rooms.jsx
@@ -0,0 +1,78 @@
+export function RoomA() {
+  return <a href="">Room A</a>
+}
+export function RoomB() {
+  return <a href="">Room B</a>
+}
+export function RoomC() {
+  return <a href="">Room C</a>
+}
+export function RoomD() {
+  return <a href="">Room D</a>
+}
+export function RoomE() {
+  return <a href="">Room E</a>
+}
+export function RoomF() {
+  return <a href="">Room F</a>
+}
+export function RoomG() {
+  return <a href="">Room G</a>
+}
+export function RoomH() {
+  return <a href="">Room H</a>
+}
+export function RoomI() {
+  return <a href="">Room I</a>
+}
+export function RoomJ() {
+  return <a href="">Room J</a>
+}
+export function RoomK() {
+  return <a href="">Room K</a>
+}
+export function RoomL() {
+  return <a href="">Room L</a>
+}
+export function RoomM() {
+  return <a href="">Room M</a>
+}
+export function RoomN() {
+  return <a href="">Room N</a>
+}
+export function RoomO() {
+  return <a href="">Room O</a>
+}
+export function RoomP() {
+  return <a href="">Room P</a>
+}
+export function RoomQ() {
+  return <a href="">Room Q</a>
+}
+export function RoomR() {
+  return <a href="">Room R</a>
+}
+export function RoomS() {
+  return <a href="">Room S</a>
+}
+export function RoomT() {
+  return <a href="">Room T</a>
+}
+export function RoomU() {
+  return <a href="">Room U</a>
+}
+export function RoomV() {
+  return <a href="">Room V</a>
+}
+export function RoomW() {
+  return <a href="">Room W</a>
+}
+export function RoomX() {
+  return <a href="">Room X</a>
+}
+export function RoomY() {
+  return <a href="">Room Y</a>
+}
+export function RoomZ() {
+  return <a href="">Room Z</a>
+}
